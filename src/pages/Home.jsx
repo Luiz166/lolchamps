9d7d3023b75161ae20c33a27fa1c7bedc962c0a6
Luiz Otavio <otavio.tata.morais@gmail.com>
2023-04-27 20:10:25 -0300

    fetching data, showing champions name on component card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,37 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../components/Navbar';
+import { Container, Grid } from '@mui/material';
+import ChampionCard from '../components/ChampionCard';
 
 const Home = () => {
 
-    const [champions, setChampions] = useState;
+    const [champions, setChampions] = useState([]);
 
-    axios
-    .get('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
-    .then((res) => (Object.keys(res.data.data)))
-    .catch((err) => console.log(err));
+    const getChampions = () => {
+        axios
+        .get('http://ddragon.leagueoflegends.com/cdn/13.8.1/data/en_US/champion.json')
+        .then((res) => setChampions(Object.keys(res.data.data)))
+        .catch((err) => console.log(err));
+    }
+
+    useEffect(() =>{
+        getChampions();
+    }, []);
 
   return (
-    <NavBar/>
+    <div>
+        <NavBar/>
+        <Container maxWidth='xl'>
+            <Grid container>
+                {champions.map((champion, index) => {
+                    return <Grid key={index} item xs={3}>
+                        <ChampionCard name={champion}/>
+                    </Grid>
+                })};
+            </Grid>
+        </Container>
+    </div>
   )
 }
 
