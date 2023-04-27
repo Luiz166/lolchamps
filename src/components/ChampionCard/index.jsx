f9213ff96a7c10483ad5823aaf726afd751951db
Luiz Otavio <otavio.tata.morais@gmail.com>
2023-04-27 20:10:04 -0300

    removing description (maybe temporary) and button, setting parameters

--- src/components/ChampionCard/index.jsx
+++ src/components/ChampionCard/index.jsx
@@ -5,9 +5,9 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
-export default function ChampionCard() {
+export default function ChampionCard( {name} ) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345, marginTop: '2em' }}>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -17,19 +17,19 @@ export default function ChampionCard() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {name}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          {/* <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
-          </Typography>
+          </Typography> */}
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      {/* <CardActions>
         <Button size="small" color="primary">
           Share
         </Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 }
